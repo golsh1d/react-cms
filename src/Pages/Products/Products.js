ebb4bf9be1867ca7fa3f2044489225a7f770414f
golshid <golsheedd@gmail.com>
2025-01-26 08:56:58 +0330

    compelete product page

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,8 +1,88 @@
-import React from 'react'
-import "./Products.css"
+import React from "react";
+import "./Products.css";
+import { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import { DataGrid } from "@mui/x-data-grid";
+import { Link } from "react-router-dom";
 
 export default function Products() {
+  const [productsData, setProductsData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/products/")
+      .then((res) => res.json())
+      .then((data) => {
+        setProductsData(data);
+      });
+  }, []);
+
+  function removeProduct(productID) {
+    let newProducts = productsData.filter(product => {
+      return product.id !== productID
+    })
+
+    setProductsData(newProducts)
+  }
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "name",
+      headerName: "Name",
+      width: 250,
+      editable: true,
+      renderCell: (params) => {
+        return (
+          <Link to="{`/product/${params.row.id}">
+            <div className="data-grid-name">
+              <img src={params.row.img} alt="" className="data-grid-img" />
+              {params.row.title}
+            </div>
+          </Link>
+        );
+      },
+    },
+    {
+      field: "price",
+      headerName: "Price",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "btn",
+      headerName: "Actions",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <div className="data-grid-btn-wrapper">
+            <Link to={`/product/${params.row.id}`} className="data-grid-edit-btn-wrapper">
+              <button className="data-grid-btn edit-btn">Edit</button>
+            </Link>
+            <button className="data-grid-btn remove-btn" onClick={() => removeProduct(params.row.id)}>Remove</button>
+          </div>
+        );
+      },
+    },
+  ];
+
   return (
-    <div>Products</div>
-  )
+    <>
+      <Box sx={{ height: "88vh", width: "100%" }}>
+        <DataGrid
+          rows={productsData}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 3,
+              },
+            },
+          }}
+          pageSizeOptions={[3]}
+          checkboxSelection
+          disableRowSelectionOnClick
+        />
+      </Box>
+    </>
+  );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import Home from "./Pages/Home/Home"
 import Users from "./Pages/Users/Users"
 import NewUser from "./Pages/NewUser/NewUser"
-import Products from "./Pages/Product/Product"
+import Products from "./Pages/Products/Products"
 import Product from "./Pages/Product/Product"
 
 let routes = [
